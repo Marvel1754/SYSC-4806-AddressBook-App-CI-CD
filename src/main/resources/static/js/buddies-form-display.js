7325874785d2ac2886ec75c05d8d7d18c0c3fc30
Marvel <m4rvelados1000@gmail.com>
2025-10-24 12:29:41 -0400

    Feature: Add button to reset form on the add buddy page.
    Refactor: Change formatting of response from manual text transformation to using element objects.

--- src/main/resources/static/js/buddies-form-display.js
+++ src/main/resources/static/js/buddies-form-display.js
@@ -1,7 +1,7 @@
 function addAndShowBuddy(addressBookName) {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState === 4 && this.status === 200) {
             document.getElementById("response-preamble").hidden = false;
             document.getElementById("add-buddies-table").innerHTML = convertToSingleRow(this.responseText);
         }
@@ -21,7 +21,7 @@ function addAndShowBuddy(addressBookName) {
 function removeBuddyOptions(addressBookName) {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState === 4 && this.status === 200) {
             document.getElementById("id").innerHTML = convertToOptions(this.responseText);
         }
     }
@@ -33,9 +33,10 @@ function removeBuddyOptions(addressBookName) {
 function removeAndShowBuddy(addressBookName) {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState === 4 && this.status === 200) {
             document.getElementById("response-preamble").hidden = false;
             document.getElementById("remove-buddies-table").innerHTML = convertToSingleRow(this.responseText);
+            removeBuddyOptions(addressBookName);
         }
     }
     let form = document.getElementById("remove-buddy-form");
@@ -50,7 +51,7 @@ function removeAndShowBuddy(addressBookName) {
 function showBuddies(addressBookName) {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState === 4 && this.status === 200) {
             document.getElementById("buddies-table").innerHTML = convertToTable(this.responseText);
         }
     }
@@ -61,49 +62,98 @@ function showBuddies(addressBookName) {
 
 function convertToTable(jsonString) {
     let responseList = JSON.parse(jsonString);
-    var table = "";
-    var tableHeader = "<tr><th>Id</th><th>Firstname</th><th>Lastname</th><th>Contact</th><th>Address</th></tr>";
+    let table = document.createElement("table");
+    table.border = "1";
 
-    table += "<table border='1'>" + tableHeader;
+    let tableHeaderRow = document.createElement("tr");
+
+    let tableHeaderId = document.createElement("th");
+    tableHeaderId.textContent = "Id";
+    let tableHeaderFirstname = document.createElement("th");
+    tableHeaderFirstname.textContent = "Firstname";
+    let tableHeaderLastname = document.createElement("th");
+    tableHeaderLastname.textContent = "Lastname";
+    let tableHeaderContact = document.createElement("th");
+    tableHeaderContact.textContent = "Contact";
+    let tableHeaderAddress = document.createElement("th");
+    tableHeaderAddress.textContent = "Address";
+
+    tableHeaderRow.append(tableHeaderId, tableHeaderFirstname, tableHeaderLastname, tableHeaderContact, tableHeaderAddress);
+
+    table.appendChild(tableHeaderRow);
+
+    let tableRow, tableRowId, tableRowFirstname, tableRowLastname, tableRowContact, tableRowAddress;
     for (var i = 0; i < responseList.length; i++) {
-        table += "<tr>" +
-            "<td>" + responseList[i].id + "</td>" +
-            "<td>" + responseList[i].firstname + "</td>" +
-            "<td>" + responseList[i].lastname + "</td>" +
-            "<td>" + responseList[i].phoneNumber + "</td>" +
-            "<td>" + responseList[i].address + "</td>" +
-            "</tr>";
+        tableRow = document.createElement("tr");
+
+        tableRowId = document.createElement("td");
+        tableRowId.textContent = responseList[i].id;
+        tableRowFirstname = document.createElement("td");
+        tableRowFirstname.textContent = responseList[i].firstname;
+        tableRowLastname = document.createElement("td");
+        tableRowLastname.textContent = responseList[i].lastname;
+        tableRowContact = document.createElement("td");
+        tableRowContact.textContent = responseList[i].phoneNumber;
+        tableRowAddress = document.createElement("td");
+        tableRowAddress.textContent = responseList[i].address;
+
+        tableRow.append(tableRowId, tableRowFirstname, tableRowLastname, tableRowContact, tableRowAddress);
+
+        table.appendChild(tableRow);
     }
-    table += "</table>";
-    console.log(table);
-    return table;
+
+    console.log(table.outerHTML);
+    return table.outerHTML;
 }
 
 function convertToSingleRow(jsonString) {
     let response = JSON.parse(jsonString);
-    var table = "";
-    var tableHeader = "<tr><th>Firstname</th><th>Lastname</th><th>Contact</th><th>Address</th></tr>";
+    let table = document.createElement("table");
+    table.border = "1";
 
-    table += "<table border='1'>" + tableHeader;
-    table += "<tr>" +
-        "<td>" + response.firstname + "</td>" +
-        "<td>" + response.lastname + "</td>" +
-        "<td>" + response.phoneNumber + "</td>" +
-        "<td>" + response.address + "</td>" +
-        "</tr>";
-    table += "</table>";
-    console.log(table);
-    return table;
+    let tableHeaderRow = document.createElement("tr");
+
+    let tableHeaderFirstname = document.createElement("th");
+    tableHeaderFirstname.textContent = "Firstname";
+    let tableHeaderLastname = document.createElement("th");
+    tableHeaderLastname.textContent = "Lastname";
+    let tableHeaderContact = document.createElement("th");
+    tableHeaderContact.textContent = "Contact";
+    let tableHeaderAddress = document.createElement("th");
+    tableHeaderAddress.textContent = "Address";
+
+    tableHeaderRow.append(tableHeaderFirstname, tableHeaderLastname, tableHeaderContact, tableHeaderAddress);
+
+    let singleRow = document.createElement("tr");
+
+    let singleRowFirstname = document.createElement("td");
+    singleRowFirstname.textContent = response.firstname;
+    let singleRowLastname = document.createElement("td");
+    singleRowLastname.textContent = response.lastname;
+    let singleRowContact = document.createElement("td");
+    singleRowContact.textContent = response.phoneNumber;
+    let singleRowAddress = document.createElement("td");
+    singleRowAddress.textContent = response.address;
+
+    singleRow.append(singleRowFirstname, singleRowLastname, singleRowContact, singleRowAddress);
+
+    table.appendChild(tableHeaderRow);
+    table.appendChild(singleRow);
+
+    console.log(table.outerHTML);
+    return table.outerHTML;
 }
 
 function convertToOptions(jsonString) {
     let responseList = JSON.parse(jsonString);
-    var options = "";
+    let options = document.createElement("select");
 
-    for (var i = 0; i < responseList.length; i++) {
-        options += "<option value=" + responseList[i].id + ">" +
-            responseList[i].id + " | " + responseList[i].firstname + " | " + responseList[i].lastname + " | " + responseList[i].phoneNumber +
-            "</option>"
+    let option;
+    for (let i = 0; i < responseList.length; i++) {
+        option = document.createElement("option");
+        option.value = responseList[i].id;
+        option.textContent = responseList[i].id + " | " + responseList[i].firstname + " | " + responseList[i].lastname + " | " + responseList[i].phoneNumber;
+        options.appendChild(option);
     }
-    return options;
+    return options.innerHTML;
 }
